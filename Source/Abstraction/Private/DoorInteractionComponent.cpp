5e7673d07fb2e0d85b4c33da704e8e0c2b4a2adb
Nikolas Kazanjian <nikolas.kazanjiann@gmail.com>
2024-03-21 19:30:36 +0200

    Added A debugger and a blueprint widget
    We can check the different states with the debug
    There is now an objective widget that disappears after 2s

--- Source/Abstraction/Private/DoorInteractionComponent.cpp
+++ Source/Abstraction/Private/DoorInteractionComponent.cpp
@@ -7,14 +7,23 @@
 #include "Engine/TriggerBox.h"
 #include "Engine/World.h"
 
+#include "DrawDebugHelpers.h"
+
+constexpr float FTL_METERS(float meters) { return meters * 100.0f; }
+
+static TAutoConsoleVariable<bool> CVarToggleDebugDoor(
+	TEXT("Abstraction.DoorInteractionComponent.Debug"),
+	false,
+	TEXT("Toggle DoorInteractionComponent debug display."),
+	ECVF_Default);
+
 // Sets default values for this component's properties
 UDoorInteractionComponent::UDoorInteractionComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
+	DoorState = EDoorState::DS_Closed;
 
-	// ...
+	CVarToggleDebugDoor.AsVariable()->SetOnChangedCallback(FConsoleVariableDelegate::CreateStatic(&UDoorInteractionComponent::OnDebugToggled));
 }
 
 // Called when the game starts
@@ -59,5 +68,22 @@ void UDoorInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickTy
 			GetOwner()->SetActorRotation(CurrentRotation);
 		}
 	}
+
+	DebugDraw();
 }
 
+void UDoorInteractionComponent::OnDebugToggled(IConsoleVariable* Var)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnDebugToggle"));
+}
+
+void UDoorInteractionComponent::DebugDraw()
+{
+	if (CVarToggleDebugDoor->GetBool())
+	{
+		FVector Offset(FTL_METERS(-0.75f), 0.0f, FTL_METERS(2.5f));
+		FVector StartLocation = GetOwner()->GetActorLocation() + Offset;
+		FString EnumAsString = TEXT("Door State: ") + UEnum::GetDisplayValueAsText(DoorState).ToString();
+		DrawDebugString(GetWorld(), Offset, EnumAsString, GetOwner(), FColor::Blue, 0.0f);
+	}
+}
--- Source/Abstraction/Public/DoorInteractionComponent.h
+++ Source/Abstraction/Public/DoorInteractionComponent.h
@@ -8,6 +8,15 @@
 #include "DoorInteractionComponent.generated.h"
 
 class ATriggerBox;
+class IConsoleVariable;
+
+UENUM()
+enum class EDoorState
+{
+	DS_Closed = 0 UMETA(DisplayName = "Closed"),
+	DS_Open = 1 UMETA(DisplayName = "Open"),
+	DS_Locked = 2 UMETA(DisplayName = "Locked"),
+};
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ABSTRACTION_API UDoorInteractionComponent : public UActorComponent
@@ -21,6 +30,8 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	static void OnDebugToggled(IConsoleVariable* Var);
+	void DebugDraw();
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
@@ -41,4 +52,7 @@ protected:
 
 	UPROPERTY(EditAnywhere)
 	FRuntimeFloatCurve OpenCurve;
+
+	UPROPERTY(BlueprintReadOnly)
+	EDoorState DoorState;
 };
